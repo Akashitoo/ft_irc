f4ab9bf2c4d49e8313c47823a19d3c03626fb2c6
Akash Balasubramaniam <abalasub@f2r7s7.clusters.42paris.fr>
2025-06-11 15:02:50 +0200

    debut du serv, connexion possible

--- ft_irc.cpp
+++ ft_irc.cpp
@@ -13,7 +13,27 @@ int main(int argc, char **argv)
 	ipport.sin_addr.s_addr = inet_addr("127.0.0.1");
 	if (bind(sock, (struct sockaddr *)&ipport, sizeof(ipport)) == -1)
 		std::cerr << "Error : Failed to bind socket\n";
+	if (listen(sock, 10) == -1)
+		std::cerr << "Error : Failed to listen socket\n";
+	std::vector<int> client_fds;
+	std::vector<struct sockaddr_in> client_info;
+	while (true)
+	{
+		sockaddr_in client;
+		socklen_t client_size = sizeof(client);
+		int clientfd = accept(sock, (struct sockaddr *)&client, &client_size);
+		if (clientfd == -1)
+			std::cerr << "a client failed to connect \n";
+		else
+		{
+			std::cout << "connexion etabli !! \n";
+		}
+		client_fds.push_back(clientfd);
+		client_info.push_back(client);
+	}
 
+	for (size_t i=0; i < client_fds.size(); i++)
+		close(client_fds[i]);
 	close(sock);
 	(void) argc;
 	(void) argv;
